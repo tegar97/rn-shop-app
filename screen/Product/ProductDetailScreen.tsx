243741de6754d4167b209f10853bd58658203bf7
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-12 23:52:52 +0700

    dynamic header

--- screen/Product/ProductDetailScreen.tsx
+++ screen/Product/ProductDetailScreen.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { View, Text, Image, Button, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
-const ProductDetailScreen = () => {
+const ProductDetailScreen = (props: any) => {
+  const productId = props.navigation.getParam("productId");
+  const selectedProduct = useSelector((state: any) =>
+    state?.products?.availableProduct.find((prod: any) => prod.id === productId)
+  );
+
   return (
     <View>
       <Text>Product Detail Screen</Text>
     </View>
   );
+};
+
+ProductDetailScreen.navigationOptions = (navData: any) => {
+  return {
+    headerTitle: navData.navigation.getParam("productTitle"),
+  };
 };
 export default ProductDetailScreen;
