7ebc98688200242a54ac253627641031e9ec4613
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-12 23:09:05 +0700

    product items style

--- screen/Product/ProductOverviewScreen.tsx
+++ screen/Product/ProductOverviewScreen.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 import rootReducer from "./../../reducers/product";
+
 const ProductOverviewScreen: React.FC = () => {
   const products = useSelector(
     (state: any) => state?.products?.availableProduct
@@ -12,7 +14,14 @@ const ProductOverviewScreen: React.FC = () => {
       data={products}
       keyExtractor={(item) => item.id}
       renderItem={(itemData) => {
-        return <Text>{itemData.item.title}</Text>;
+        return (
+          <ProductItem
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+            onViewDetail={() => console.log("yo")}
+          />
+        );
       }}
     />
   );
